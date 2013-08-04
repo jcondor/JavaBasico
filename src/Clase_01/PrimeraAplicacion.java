1d52c048c0576d0ba38dccf1f05f1d9fb94fd913
PFR <PFR@LAB605-10>
2013-08-04 14:26:21 -0500

    Clase1

---
+++ src/Clase_01/PrimeraAplicacion.java
@@ -0,0 +1,64 @@
+package Clase_01;
+
+public class PrimeraAplicacion {
+
+    public static void main(String[] args) {
+        PrimeraAplicacion.ejercicio8();
+    }
+
+    public static void ejercicio1() {
+        int resultado = 7 * (15 + 1) * 2 % 2;
+        System.out.println(resultado);
+    }
+
+    public static void ejercicio2() {
+        boolean resultado = 0.0 / 0.0 == 0.0 / 0.0;
+        System.out.println(resultado);
+    }
+
+    public static void ejercicio3() {
+        boolean resultado = (!true && false);
+        System.out.println(resultado);
+    }
+
+    public static void ejercicio4() {
+        boolean resultado = ((3 * 4 >= 12) && (3.5 > 1 + 2f)) || (4 < 8);
+        System.out.println(resultado);
+    }
+
+    public static void ejercicio5() {
+        boolean resultado = !(true == false);
+        System.out.println(resultado);
+    }
+
+    public static void ejercicio6() {
+        boolean resultado = (!true == false);
+        System.out.println(resultado);
+    }
+
+    public static void ejercicio7() {
+        int edad = 5;
+        int resultado = ((edad >= 0) && (edad < 18)) ? 15
+                : ((edad >= 18) && (edad < 60)) ? 0
+                : (edad >= 60) ? 30 : 0;
+        System.out.println(resultado);
+    }
+
+    public static void ejerciciomatriz() {
+        int[] matriznumero = {2, 24, 5, 6};
+
+        for (int i = 0; i < matriznumero.length; i++) {
+            System.out.println(matriznumero[i]);
+        }
+
+    }
+
+    public static void ejercicio8() {
+        int[] meses = {31,28,31,30,31,30,31,30,31,30,31,30};
+
+        for (int i = 0; i < meses.length; i++) {
+            System.out.println(meses[i]);
+        }
+
+    }
+}
